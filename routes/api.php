099e0c0b2b1688733084baa1c265a0984c375e65
João <joao.pedrolopes10@hotmail.com>
2022-08-17 02:52:24 -0300

    Refatoração

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
 Route::middleware(['admin'])->group(function () {
     Route::get('rank_por_movimento/{id}', 'App\Http\Controllers\ApiController@getUsersRankByMovement');
 });
-=======
-Route::get('rank_por_movimento/{id}', 'App\Http\Controllers\ApiController@getUsersRankByMovement');
->>>>>>> 9fb0a365806d887a0d91425fc0eaebf22494aee8
